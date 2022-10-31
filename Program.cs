9237346d28144603c89d605f243584130db8c73b
nolto1sj <sjnolton@gmail.com>
2022-10-31 00:21:31 -0400

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿static int SquareDigits(int n)
+{
+    string result = n.ToString();
+    var intList = result.Select(d => int.Parse(d.ToString())).ToArray();
+    for (int i = 0; i < intList.Length; i++)
+    {
+        double newNum = Math.Pow(intList[i], 2);
+        intList[i] = (int)newNum;
+    }
+    string resultArray = String.Join("", intList);
+    return int.Parse(resultArray);
+}
